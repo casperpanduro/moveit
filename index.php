ac70ddd2cd5e1fbd20212d15cb7a4f470db2c94b
casperpanduro <b0:74:e2:53:a7:d2:c8:a7:95:ba:a9:22:41:dc:28:cbb0:74:e2:53:a7:d2:c8:a7:95:ba:a9:22:41:dc:28:cbcasperj@me.com>
2016-12-09 11:19:59 +0100

    First commit

---
+++ index.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Loadit</title>
+	<link rel="stylesheet" type="text/css" href="styles.css">
+
+
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+	<link rel="stylesheet" href="https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/themes/smoothness/jquery-ui.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"></script>
+
+</head>
+<body>
+	
+	<section>
+		<div class="container">
+			<h1 class="moveit">moveit.js is <br>an awesome <br>jquery plugin</h1>
+			<h2 class="subline">GET IT</h2>
+		</div>
+	</section>
+	<section>
+		<div class="container">
+			<h1 class="moveit">moveit.js is <br>an awesome <br>jquery plugin</h1>
+			<h2 class="subline">GET IT</h2>
+		</div>
+	</section>
+	<section>
+		<div class="container">
+			<h1 class="moveit">moveit.js is <br>an awesome <br>jquery plugin</h1>
+			<h2 class="subline">GET IT</h2>
+		</div>
+	</section>
+	<section>
+		<div class="container">
+			<h1 class="moveit">moveit.js is <br>an awesome <br>jquery plugin</h1>
+			<h2 class="subline">GET IT</h2>
+		</div>
+	</section>
+	<section>
+		<div class="container">
+			<h1 class="moveit">moveit.js is <br>an awesome <br>jquery plugin</h1>
+			<h2 class="subline">GET IT</h2>
+		</div>
+	</section>
+	<section>
+		<div class="container">
+			<h1 class="moveit">moveit.js is <br>an awesome <br>jquery plugin</h1>
+			<h2 class="subline">GET IT</h2>
+		</div>
+	</section>
+	<section>
+		<div class="container">
+			<h1 class="moveit">moveit.js is <br>an awesome <br>jquery plugin</h1>
+			<h2 class="subline">GET IT</h2>
+		</div>
+	</section>
+	<section>
+		<div class="container">
+			<h1 class="moveit">moveit.js is <br>an awesome <br>jquery plugin</h1>
+			<h2 class="subline">GET IT</h2>
+		</div>
+	</section>
+	<section>
+		<div class="container">
+			<h1 class="moveit">moveit.js is <br>an awesome <br>jquery plugin</h1>
+			<h2 class="subline">GET IT</h2>
+		</div>
+	</section>
+		
+	
+	
+	<script src="moveit.js"></script>
+	<script>
+		$(document).ready(function(){
+			$(".moveit").moveit({
+				sensitivity: 10
+			});
+			$(".subline").moveit({
+				sensitivity: 20
+			})
+		});
+	</script>
+</body>
+</html>
---
+++ moveit.js
@@ -0,0 +1,46 @@
+
+// Developed by Casper Panduro
+
+(function ( $ ) {
+    $.fn.moveit = function( options ) {
+        var settings = $.extend({
+            // These are the defaults.
+            sensitivity: 10,
+            shadow: false
+        }, options );
+
+        var wWidth = $(window).width();
+        var wHeight = $(window).height();
+
+        $(window).resize(function(){
+            wWidth = $(window).width();
+            wHeight = $(window).height();
+        });
+        
+        var moveit = this;
+
+        moveit.css({
+            "position":"relative"
+        });
+
+        if(settings.shadow == true) {
+            var content = this.html();
+            this.append('<span style="position: absolute; top: 0px; left: 0px;">'+content+'</span>')
+        }
+
+        $(window).mousemove(function( event ) {
+          var yHeight = (event.pageY - $(window).scrollTop() ) / (wHeight/2) -1;
+          var yWidth = event.pageX / (wWidth/2) -1;
+          moveit.css({
+            "top": yHeight*settings.sensitivity,
+            "left":yWidth*settings.sensitivity
+          });
+        });
+
+        return this;
+    };
+ 
+}( jQuery ));
+
+
+
